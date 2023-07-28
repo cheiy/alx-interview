00486ec6b9439188334c7002172746cad28bbd2d
cheiy <gschege@gmail.com>
2023-07-28 00:35:22 +0000

    Pascal's Triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+
+
 def pascal_triangle(n):
     """
     Function returns a list of lists of integers representing the
@@ -6,3 +9,26 @@ def pascal_triangle(n):
     integers_list = []
     if n <= 0:
         return integers_list
+
+    if n == 1:
+        new_list = [1]
+        integers_list.append(new_list)
+        return integers_list
+
+    if n == 2:
+        integers_list = [[1]]
+        new_list = [1, 1]
+        integers_list.append(new_list)
+        return integers_list
+
+    triangle = [[1], [1, 1]]
+
+    for i in range(2, n):
+        temp = [1, 1]
+        for j in range(0, len(triangle[-1])-1):
+            a = triangle[-1][j]
+            b = triangle[-1][j+1]
+            temp.insert(-1, a + b)
+        triangle.append(temp)
+
+    return triangle
