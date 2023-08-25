e0ecbbad4558be96c2345863d6db5c704fc7cf11
cheiy <gschege@gmail.com>
2023-08-25 02:44:42 +0000

    More validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -13,9 +13,9 @@ def validUTF8(data):
     for char in data:
         if char == 0 or char == 00 or char == 000:
             flag.append(1)
+        elif char < 128 and char > -1:
+            flag.append(1)
         elif char % 128 == 0:
-            flag.append(0)
-        elif char % 256 == 0:
             flag.append(0)
         else:
             flag.append(1)
