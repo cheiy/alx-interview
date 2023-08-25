b3b1ff8e492c996fc4f57bec9251c434d9dfebdd
cheiy <gschege@gmail.com>
2023-08-25 02:00:13 +0000

    UTF-8 Validation

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Module contains a method/function that determines if a given data set
+represents a valid UTF-8 encoding
+"""
+
+
+def validUTF8(data):
+    """
+    Method determines if a given data set represents a valid UTF-8 encoding
+    """
+    flag = 0
+    for char in data:
+        if char % 128 == 0:
+            flag = 0
+        else:
+            flag = 1
+    if flag == 0:
+        return False
+    else:
+        return True
